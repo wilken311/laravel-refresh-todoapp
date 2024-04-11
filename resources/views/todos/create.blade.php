51da1172eaaeb9257bfc6e774f7e705c64b60e25
wm <monterowilken311@gmail.com>
2024-04-11 15:52:13 +0800

    Fixed Designs

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -12,31 +12,32 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="flex">
+                <div class="p-6 text-gray-900 dark:text-gray-100 w-full">
                     <form method="POST" action="{{ route('todos.store') }}">
                         @csrf
-                        <!-- Title -->
-                        <div>
-                            <x-input-label for="title" :value="__('Title')" />
-                            <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')"  />
-                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                        <div class="flex flex-col space-y-4">
+                            <!-- Title -->
+                            <div>
+                                <x-input-label for="title" :value="__('Title')" />
+                                <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')"  />
+                                <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                            </div>
+
+                            <!-- Description -->
+                            <div>
+                                <x-input-label for="description" :value="__('Description')" />
+                                <x-text-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')"  />
+                                <x-input-error :messages="$errors->get('description')" class="mt-2" />
+                            </div>
+
+                            <!-- Submit Button -->
+                            <div>
+                                <x-primary-button>
+                                    {{ __('Submit') }}
+                                </x-primary-button>
+                            <div>
                         </div>
-
-                         <!-- Description -->
-                         <div>
-                            <x-input-label for="description" :value="__('Description')" />
-                            <x-text-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')"  />
-                            <x-input-error :messages="$errors->get('description')" class="mt-2" />
-                        </div>
-
-
-                        <x-primary-button class="ms-3">
-                            {{ __('Submit') }}
-                        </x-primary-button>
-
-                        </form>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
